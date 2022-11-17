fd4c68c22d53963fca7918b2dca82687dc43f77b
Ofri Gal <ofri0101@gmail.com>
2022-11-17 11:34:01 +0200

    test

--- project/core/flask.py
+++ project/core/flask.py
@@ -6,3 +6,8 @@ core = Blueprint('core', __name__, template_folder='templates', url_prefix='/')
 @core.route("/")
 def index():
     return render_template('index.html') 
+
+@core.route("/test")
+def index():
+    print('{"name":"ofri", "age": 21}')
+    return '{"name":"ofri", "age": 21}'
